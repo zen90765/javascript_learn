7e2dbd4bdd7dd69cb841802042423e96490e0899
aryan098278 <aryanmalviya779@gmail.com>
2025-11-09 10:50:46 +0000

    done

--- .devcontainer/basic_01.js
+++ .devcontainer/basic_01.js
@@ -1,1 +1,8 @@
-console.log("Aryan")
+const accountId = 897
+let accountPassword="Aryan"
+accountCity="indore"
+
+console.log(accountId);
+
+console.table([accountId, accountPassword, accountCity])
+
